40a06579cc852e9d8443cff8192cd18d7eb25960
kirigiri hitomi <chenjw@hiscene.com>
2023-12-27 17:54:27 +0800

    Update machine context value

--- src/lib.rs
+++ src/lib.rs
@@ -90,10 +90,11 @@ mod test {
         let machine = pause_trigger(machine);
         println!("{:?}, {:?}", machine.context, machine.current_state);
         assert_eq!(machine.current_state, "paused");
-        assert_eq!(machine.context, 12);
+        assert_eq!(machine.context, 6);
 
         let machine = resume_trigger(machine);
         println!("{:?}, {:?}", machine.context, machine.current_state);
         assert_eq!(machine.current_state, "running");
+        assert_eq!(machine.context, 6);
     }
 }
